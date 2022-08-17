Upgrade to SPADE_HDR (v: India) (fix uint8 mistake and sun overflow)

data/base_dataset.py
@@ -43,16 +43,23 @@ def get_params(opt, size):
     flip = random.random() > 0.5
     return {'crop_pos': (x, y), 'flip': flip}
 
-
+### TODO: Change PIL transforms to torch transforms! 
+### Upgrade to HDR capabilities
 def get_transform(opt, params, method=Image.BICUBIC, normalize=False, toTensor=True, isLabel=True, rotation_angle=0):
     transform_list = []
-    
+    # First make a tensor; then do operations: rand_rotate -> resize(redundant)
+    if toTensor: 
+        transform_list += [transforms.ToTensor()]
+        # print(transform_list)
+
     if opt.rand_rotate:
         if isLabel:
             rand_rotate = np.random.randint(0,360)
-            transform_list.append(transforms.Lambda(lambda img: __rand_rotate(img, rand_rotate)))
+            # transform_list.append(transforms.Lambda(lambda img: __rand_rotate(img, rand_rotate)))
+            transform_list.append(transforms.Lambda(lambda img: transforms.functional.rotate(img, rand_rotate)))
         else:
-            transform_list.append(transforms.Lambda(lambda img: __rand_rotate(img, rotation_angle)))
+            # transform_list.append(transforms.Lambda(lambda img: __rand_rotate(img, rotation_angle)))
+            transform_list.append(transforms.Lambda(lambda img: transforms.functional.rotate(img, rotation_angle)))
 
     if 'resize' in opt.preprocess_mode:
         osize = [opt.load_size, opt.load_size]
@@ -69,22 +76,21 @@ def get_transform(opt, params, method=Image.BICUBIC, normalize=False, toTensor=T
         base = 32
         transform_list.append(transforms.Lambda(lambda img: __make_power_2(img, base, method)))
     
-    if opt.preprocess_mode == 'fixed':
+    if opt.preprocess_mode == 'fixed': 
         w = opt.crop_size
         h = round(opt.crop_size / opt.aspect_ratio)
-        # print(f"Selected h,w for resizing: {w}, {h}")
-        transform_list.append(transforms.Lambda(lambda img: __resize(img, w, h, method)))
+        osize = [h, w]
+        transform_list.append(transforms.Scale(osize, interpolation=transforms.InterpolationMode.BICUBIC))   
+        # transform_list.append(transforms.Lambda(lambda img: __resize(img, w, h, method)))
 
     if opt.isTrain and not opt.no_flip:
         transform_list.append(transforms.Lambda(lambda img: __flip(img, params['flip'])))
 
-    if toTensor: 
-        transform_list += [transforms.ToTensor()]
-        # print(transform_list)
-
     if normalize:
         transform_list += [transforms.Normalize((0.5, 0.5, 0.5),
                                                 (0.5, 0.5, 0.5))]
+
+    # print(transform_list)
     if isLabel:
         return transforms.Compose(transform_list), rand_rotate
     else:

data/pix2pix_dataset.py
@@ -95,9 +95,7 @@ class Pix2pixDataset(BaseDataset):
         if ".exr" in image_path:
             image = self.loadImage(image_path)
             # image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-            image = np.asarray(image)
-            # print(f"Img shape: {np.asarray(image).shape}")
-            image = Image.fromarray((image * 255.0).astype(np.uint8))
+            # print(image_path, " -> ", image.dtype) ### float32
         else:
             image = Image.open(image_path)
             # image = image.convert('RGB')
@@ -131,7 +129,6 @@ class Pix2pixDataset(BaseDataset):
                       'path': image_path,
                       }
         
-        ### TODO: Print sizes -- DONE
         # print(f"input_dict[image]: {input_dict['image']}")
         
         # Give subclasses a chance to modify the final output

util/util.py
@@ -61,7 +61,7 @@ def tile_images(imgs, picturesPerRow=4):
 
 # Converts a Tensor into a Numpy array
 # |imtype|: the desired type of the converted numpy array
-def tensor2im(image_tensor, imtype=np.float32, normalize=True, tile=False):
+def tensor2im(image_tensor, imtype=np.float32, normalize=False, tile=False):
     if isinstance(image_tensor, list):
         image_numpy = []
         for i in range(len(image_tensor)):
@@ -84,14 +84,23 @@ def tensor2im(image_tensor, imtype=np.float32, normalize=True, tile=False):
 
     if image_tensor.dim() == 2:
         image_tensor = image_tensor.unsqueeze(0)
+
     image_numpy = image_tensor.detach().cpu().float().numpy()
+    # print("l-89 [util.py]: Shape", image_numpy.shape, " dtype: ", image_numpy.dtype)
+
     if normalize:
         image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0
     else:
-        image_numpy = np.transpose(image_numpy, (1, 2, 0)) * 255.0
-    image_numpy = np.clip(image_numpy, 0, 255)
+        # Removed: * 255
+        image_numpy = np.transpose(image_numpy, (1, 2, 0)) 
+
+    # HDR Upgrade: No need to clip here. 
+    # (But possibly: Inverse-Tonemap -> Gamma tonemap -> Clip)
+    # image_numpy = np.clip(image_numpy, 0, 255)
+
     if image_numpy.shape[2] == 1:
         image_numpy = image_numpy[:, :, 0]
+
     return image_numpy.astype(imtype)
 
 