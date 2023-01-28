5ff1214039824975d05a33ec570af67d9baef6c6
Aryan-Garg <aryangarg019@gmail.com>
2023-01-28 04:08:32 -0500

    Superficial changes

--- export_test.sh
+++ export_test.sh
@@ -9,10 +9,10 @@ GAMMA_LABEL=1.0
 GAMMA_IMAGE=2.2
 
 /usr/bin/ffmpeg -y \
-      -gamma $GAMMA_LABEL -framerate 1 -f image2 -pattern_type glob -thread_queue_size 1024  -i "$MASK/*.png" \
-      -gamma $GAMMA_IMAGE -framerate 1 -f image2 -pattern_type glob -thread_queue_size 1024  -i "$SPADE/itmLG2_*.exr" \
+      -gamma $GAMMA_LABEL -framerate 1 -f image2 -start_number 1 -thread_queue_size 1024  -i "datasets/full_pysolar_dataset/full_test_discrete/masks_color/$2_%d.png" \
+      -gamma $GAMMA_IMAGE -framerate 1 -f image2 -start_number 1 -thread_queue_size 1024  -i "datasets/full_pysolar_dataset/full_gt_test/$2_%d.exr" \
      \
-      -vcodec libx264 -pix_fmt yuvj420p -crf 20 -preset ultrafast -filter_complex \
+      -vcodec libx265 -pix_fmt yuvj420p -crf 28 -preset ultrafast -filter_complex \
        "[0:v]drawtext=text='Mask':fontcolor=white:fontsize=24:box=1:boxcolor=black@0.5:boxborderw=5:x=text_h:y=text_h[v0]; \
         [1:v]drawtext=text='Synth':fontcolor=green:fontsize=24:box=1:boxcolor=black@0.5:boxborderw=5:x=text_h:y=text_h[v1]; \
         [v0][v1]hstack=inputs=2[v]" \
--- export_test_full_day.sh
+++ export_test_full_day.sh
@@ -2,15 +2,15 @@
 
 # VIDEO
 # Usage: ./export_vid.sh
-MASK_TYPE="results/val_masks_4/jimbo_full_100/test_latest/images/input_label"
-SPADE_OUT="results/val_masks_4/jimbo_full_100/test_latest/images/synthesized_image"
-REAL_OUT="datasets/validation/val_one_day"
+MASK_TYPE="results/t1/full_pysolar/test_100/images/input_label"
+SPADE_OUT="results/t1/full_pysolar/test_100/images/synthesized_image"
+REAL_OUT="datasets/testsets/val_one_day"
 GAMMA_LABEL=1.0
 GAMMA_IMAGE=2.2
 /usr/bin/ffmpeg -y \
       -gamma $GAMMA_LABEL -framerate 1 -f image2 -start_number 1 -thread_queue_size 1024  -i "$MASK_TYPE/20160521_%d.png" \
       -gamma $GAMMA_IMAGE -framerate 1 -f image2 -start_number 1 -thread_queue_size 1024  -i "$REAL_OUT/r_20160521_%d.exr" \
-      -gamma $GAMMA_IMAGE -framerate 1 -f image2 -start_number 1 -thread_queue_size 1024  -i "$SPADE_OUT/itmLG2_20160521_%d.exr" \
+      -gamma $GAMMA_IMAGE -framerate 1 -f image2 -start_number 1 -thread_queue_size 1024  -i "$SPADE_OUT/20160521_%d.exr" \
      \
       -vcodec libx264 -pix_fmt yuvj420p -crf 20 -preset ultrafast -filter_complex \
        "[0:v]drawtext=text='Mask':fontcolor=white:fontsize=24:box=1:boxcolor=black@0.5:boxborderw=5:x=text_h:y=text_h[v0]; \
@@ -20,4 +20,4 @@ GAMMA_IMAGE=2.2
       -map "[v]" \
       -r 1 \
       -f mov \
-      "one_full_day.mov"
+      "t1.mov"
